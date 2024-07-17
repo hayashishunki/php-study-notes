2c6acc1415292eb7535d7bcf421a08e3dc659f2b
hayashi <s.y1014torimaru@gmail.com>
2024-07-17 22:41:21 +0900

    Printing Variable Information.

--- questionList/question02.php
+++ questionList/question02.php
@@ -10,4 +10,16 @@ $str = "あいうえお";
 $num = -100;
 $d = 22.4;
 echo "{$str}\n{$num}\n{$d}";
+
+echo "\n";
+// ③変数値の画面出力
+echo "{$str1}\n{$num1}";
+
+echo "\n";
+// 変数情報の出力
+$msg = "good morning";
+$num2 = 1234;
+$root = 1.41421356;
+$flg2 = false;
+var_dump($msg, $num2, $root, $flg2);
 ?>
