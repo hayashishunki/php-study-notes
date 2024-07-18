fe60cf1e4718188bc3a92ccea665517b806ca318
hayashi <s.y1014torimaru@gmail.com>
2024-07-18 23:09:16 +0900

    Comparison Operators & Code shaping.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-■次回開始
+■2024/07/17時点の次回開始
 https://sls.ideal-growth.jp/12/lesson/4/learning
+
+■2024/07/17時点次回開始
+https://sls.ideal-growth.jp/12/lesson/292/learning
--- questionList/question04.php
+++ questionList/question04.php
@@ -18,5 +18,19 @@ echo "\n";
 // ③ 文字列連結
 $str1 = "a";
 $str1 .= "bcde";
-echo $str1;
+echo $str1, "\n";
+
+echo "\n";
+
+// ④ 比較演算子
+$num1 = 10;
+$num2 = 10;
+$d1 = 5.5;
+$bool1 = $num1 === $num2;
+$bool2 = $num1 !== $num2;
+$bool3 = $num1 < $d1;
+$bool4 = $num1 <= $num2;
+$bool5 = $d1 > $num1;
+$bool6 = $num2 >= $d1;
+var_dump($bool1, $bool2, $bool3, $bool4, $bool5, $bool6);
 ?>
