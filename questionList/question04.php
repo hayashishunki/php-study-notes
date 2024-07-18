eb64f6f566a88569b9f48111b97fa61fe1eba073
hayashi <s.y1014torimaru@gmail.com>
2024-07-18 22:44:20 +0900

    Four arithmetic operations.

---
+++ questionList/question04.php
@@ -0,0 +1,15 @@
+<?php
+// 四則演算
+// ①
+echo $add = 1 + 1, "\n";
+echo $sub = 5 - 2, "\n";
+echo $mul = 5 * 3, "\n";
+echo $div = 4 / 2, "\n";
+
+echo "\n";
+
+// ②
+echo 5 % 2, "\n";
+echo 23 % 5, "\n";
+echo 1234 % 10, "\n";
+?>
