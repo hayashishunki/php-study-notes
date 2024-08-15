05edb2b9b72563ba8f64d1ddbdaa0272f1ea9792
hayashi <101466606+hayashishunki@users.noreply.github.com>
2024-08-16 07:49:08 +0900

    Update question07.php
    Last question add.

--- questionList/question07.php
+++ questionList/question07.php
@@ -39,4 +39,25 @@ if(($otonaNizu + $kodomoNinzu) >= 5) {
 
 echo "合計金額は{$goukei}円です。";
 
+
+// 応用課題-遊園地のチケット券売機を作ろう④
+$otonaKakaku = 7530;
+$kodomoKakaku = 4860;
+$otonaNizu = 1;
+$kodomoNinzu = 0;
+$goukei = $otonaKakaku * $otonaNizu + $kodomoKakaku * $kodomoNinzu;
+
+
+if($kodomoNinzu >= 3) {
+  $goukei -= $kodomoKakaku * (int)($kodomoNinzu / 3); 
+} else if(($otonaNizu + $kodomoNinzu) >= 5) {
+  $goukei = floor($goukei - ($goukei * 0.05));
+} else if($goukei >= 10000) {
+  $goukei -= 500;
+} else {
+  // 何もしない
+}
+
+echo "合計金額は{$goukei}円です。";
+
 ?>
