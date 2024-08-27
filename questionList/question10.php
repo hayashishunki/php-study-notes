ea8a73cada5627a2dd618e6c89c0be1198e6a1aa
hayashi <s.y1014torimaru@gmail.com>
2024-08-27 21:07:19 +0900

    break continue process add.

---
+++ questionList/question10.php
@@ -0,0 +1,19 @@
+<?php
+
+// // continue
+// for($i = 0;$i < 5;$i++) {
+//   if($i == 2) {
+//     echo "スキップ\n";
+//     continue;
+//   }
+//   echo 1 . "\n";
+// }
+
+// break
+for($i = 0;$i < 5;$i++) {
+  if($i == 2) {
+    echo "終了";
+    break;
+  }
+  echo 1 . "\n";
+}
