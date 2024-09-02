2a9138520c619852d790cb28dfa66f3214273327
hayashi <s.y1014torimaru@gmail.com>
2024-09-03 00:00:31 +0900

    Calculator Creation last add.

--- questionList/question11.php
+++ questionList/question11.php
@@ -29,37 +29,42 @@ while(1) {
 
 // ③計算機作成
 $result = 0;
-$flag = false;
-while(1) {
+
+while (1) {
   echo "入力してください([+] 足し算 [-] 引き算 [=] 計算結果 )\n";
   $operator = trim(fgets(STDIN));
 
-  if($operator == "+" || $operator == "-" || $operator == "=") {
-    $flag = true;
+  if ($operator == "+") {
+    // 加算処理
+    while (1) {
+      echo "数字を入力してください(マイナス値不可)\n";
+      $num = trim(fgets(STDIN));
+      if (is_numeric($num) && $num >= 0) {
+        $result += $num;
+        echo "合計: " . $result . "\n";
+        break;
+      } else {
+        echo "再入力\n";
+      }
+    }
+  } else if ($operator == "-") {
+    // 減算処理
+    while (1) {
+      echo "数字を入力してください(マイナス値不可)\n";
+      $num = trim(fgets(STDIN));
+      if (is_numeric($num) && $num >= 0) {
+        $result -= $num;
+        echo "合計: " . $result . "\n";
+        break;
+      } else {
+        echo "再入力\n";
+      }
+    }
+  } else if ($operator == "=") {
+    // 結果表示、処理終了
+    echo "計算結果は " . $result . " です。\n";
+    break;
   } else {
     echo "再入力\n";
-    continue;
   }
-
-  if($flag == true) {
-    if($operator == "+") {
-      echo "数字を入力してください(マイナス値不可)\n";
-      $num = trim(fgets(STDIN));
-      if(is_numeric($num) && $num) {
-
-      }
-      $result += $num;
-      echo "合計: " . $result . "\n";
-    } else if($operator == "-") {
-      echo "数字を入力してください(マイナス値不可)\n";
-      $num = trim(fgets(STDIN));
-      $result -= $num;
-      echo "合計: " . $result . "\n";
-    } else {
-      echo "計算結果は " . $result . " です。\n";
-      break;
-    }
-  }
-
-  $flag = false;
 }
--- sample.php
+++ sample.php
@@ -1,21 +1,42 @@
 <?php
 
 $result = 0;
-while(1) {
+
+while (1) {
   echo "入力してください([+] 足し算 [-] 引き算 [=] 計算結果 )\n";
   $operator = trim(fgets(STDIN));
 
-  if($operator == "+") {
-    echo "数字を入力してください\n";
-    $num = trim(fgets(STDIN));
-    $result += $num;
-    echo "合計: " . $result . "\n";
-  } else if($operator == "-") {
-    $num = trim(fgets(STDIN));
-    $result -= $num;
-    echo "合計: " . $result . "\n";
-  } else {
+  if ($operator == "+") {
+    // 加算処理
+    while (1) {
+      echo "数字を入力してください(マイナス値不可)\n";
+      $num = trim(fgets(STDIN));
+      if (is_numeric($num) && $num >= 0) {
+        $result += $num;
+        echo "合計: " . $result . "\n";
+        break;
+      } else {
+        echo "再入力\n";
+      }
+    }
+  } else if ($operator == "-") {
+    // 減算処理
+    while (1) {
+      echo "数字を入力してください(マイナス値不可)\n";
+      $num = trim(fgets(STDIN));
+      if (is_numeric($num) && $num >= 0) {
+        $result -= $num;
+        echo "合計: " . $result . "\n";
+        break;
+      } else {
+        echo "再入力\n";
+      }
+    }
+  } else if ($operator == "=") {
+    // 結果表示、処理終了
     echo "計算結果は " . $result . " です。\n";
     break;
+  } else {
+    echo "再入力\n";
   }
 }
