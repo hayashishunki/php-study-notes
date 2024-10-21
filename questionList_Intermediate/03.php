710b50007dc030d8e5c49fc8b871b37421f38767
hayashi <s.y1014torimaru@gmail.com>
2024-10-21 22:15:57 +0900

    Built-in Functions add.

---
+++ questionList_Intermediate/03.php
@@ -0,0 +1,37 @@
+<?php
+
+// ■ビルトイン関数例
+$text = "ABACADAE";
+$replace = str_replace("A", "Z", $text, $count);
+echo $replace; // ZBZCZDZE
+echo $count; // 4
+
+echo "\n";
+
+$text2 = "ABCDE";
+var_dump(str_contains($text2, "A")); // true
+var_dump(str_contains($text2, "F")); // false
+
+
+$text3 = "ABCDE";
+var_dump(strpos($text3, "A")); // 0
+var_dump(strpos($text3, "B")); // 1
+var_dump(strpos($text3, "C")); // 2
+var_dump(strpos($text3, "D")); // 3
+var_dump(strpos($text3, "E")); // 4
+var_dump(strpos($text3, "F")); // false
+
+// 大文字小文字の区別も行います
+var_dump(strpos($text3, "a")); // false
+
+
+$text4 = "ABCDEFGHIJKLMN";
+$subString = substr($text4, 2, 3);
+echo $subString . "\n"; // CDE
+
+
+// ①ビルトイン関数応用
+$str = "abcdz";
+
+$ret = str_replace("z", "e", $str);
+echo $ret;
--- sample.php
+++ sample.php
@@ -1,49 +1,6 @@
 <?php
 
-$kokugoTest = array(
-  "taro" => 65, // 太郎くんの点数は65点
-  "hanako" => 95, // 花子さんの点数は95点
-  "ichiro" => 33 // 一郎くんの点数は33点
-);
+$str = "abcdz";
 
-// 合計
-function goukei($tensu): int
-{
-  $ret = 0;
-  foreach ($tensu as $key => $val) {
-    $ret += $tensu[$key];
-  }
-
-  return $ret;
-}
-
-// 平均
-function heikin($tensu) : int {
-  $ret = $tensu / 3;
-  return $ret;
-}
-
-// 最高値
-function saidai($tensu) : int {
-  $saidai = 0;
-  $max = 0;
-  foreach($tensu as $key => $val) {
-
-    if($max == 0) {
-      $max = $tensu[$key];
-    }
-
-    if($tensu[$key] > $max) {
-      $max = $tensu[$key];
-    }
-  }
-  $saidai = $max;
-
-  return $saidai;
-}
-
-
-// $total = goukei($kokugoTest);
-// $average = heikin($total);
-$saidai = saidai($kokugoTest);
-echo "最高得点: "  . $saidai . "点";
+$ret = str_replace("z", "e", $str);
+echo $ret;
